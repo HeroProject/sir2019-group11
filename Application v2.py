0a57fdc255b28e43d71aecafcd23eea839313028
sNikip <57754266+sNikip@users.noreply.github.com>
2019-12-12 18:15:19 +0100

    Add files via upload

--- Application v2.py
+++ Application v2.py
@@ -1,5 +1,130 @@
 import AbstractApplication as Base
 from threading import Semaphore
+import copy
+from random import sample
+import pickle
+import random
+
+
+class Word:
+    def __init__(self, word, translation, sentence=None, sentence_translation=None):
+        self.word = word
+        self.translation = translation
+        self.sentence = sentence
+        self.sentence_translation = sentence_translation
+
+
+class Lesson:
+    def __init__(self, name, words):
+        self.name = name
+        self.words_to_learn = words
+        self.learned_words = []
+
+    def append(self, word):
+        '''
+        Append a word or words to a lesson.
+        Input: Instance of the class Word, or list of instances of the class Word
+        Output: None
+        '''
+
+        if type(word) == list:
+            self.words_to_learn.extend(word)
+        else:
+            self.words_to_learn.append(word)
+
+    def sample(self):
+        '''
+        Sample from a new word from the words that are not yet learned.
+        Input: None
+        Output: Instance of the class Word
+        '''
+        if len(self.words_to_learn) > 0:
+            return random.sample(self.words_to_learn, 1)[0]
+        else:
+            print('All words are learned')
+            return
+
+    def get(self, aword):
+        '''
+        Get a word from the words that are not yet learned.
+        Input: English or Dutch word to learn as a string
+        Output: Insance of the class Word corresponding to the word
+        you requested iff the word is part of the lesson and not yet learned
+        '''
+        for word in self.words_to_learn:
+            if (aword.lower() == word.word.lower()) or (aword.lower() == word.translation.lower()):
+                return word
+
+        print('Word is either already learned or is not part of this lesson.')
+
+    def correct_answer(self, word):
+        '''
+        Call when a word is correctly learned. This function removes
+        the word from the words to learn and adds it to the learned words.
+        Input: Instance of the class Word or the english word as string
+        Output: None
+        '''
+        if word in self.words_to_learn:
+            self.words_to_learn.remove(word)
+            self.learned_words.append(word)
+        elif type(word) == str:
+            self.learned_words.append(self.get(word))
+            self.words_to_learn.remove(self.get(word))
+        else:
+            print('Word is either already learned or is not part of this lesson.')
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.firstlesson = True
+        self.lessons_to_learn = copy.deepcopy(lessons)
+        self.lessons_learned = []
+        self.words_remembered = []
+
+    def update(self):
+        '''
+        Check for every lesson if there are words left that arent learned yet.
+        And updates the lessons_learned and lessons_to_learn properties.
+        Input = None
+        Output = None
+        '''
+        for lesson in self.lessons_to_learn:
+            if len(lesson.words_to_learn) == 0:
+                self.lessons_learned.append(lesson)
+                self.lessons_to_learn.remove(lesson)
+
+
+bike = Word('bicycle', 'fiets', 'I have a red bicycle', 'Ik heb een rode fiets')
+car = Word('car', 'auto', 'A car has four wheels', 'Een auto heeft vier wielen')
+train = Word('train', 'trein', 'The train leaves the station', 'De trein verlaat het station')
+plane = Word('plane', 'vliegtuig', 'There is a plane in the air', 'Er is een vliegtuig in de lucht')
+bus = Word('bus', 'bus', 'The bus drives through the city', 'De bus rijdt door de stad')
+subway = Word('subway', 'metro', 'The subway is driving underground', 'De metro rijdt onder de grond')
+
+mother = Word('mother', 'moeder', 'My mother is very smart', 'Mijn moeder is erg slim')
+father = Word('father', 'vader', 'My father has a car', 'Mijn vader heeft een auto')
+sister = Word('sister', 'zus', 'My sister is older than me', 'Mijn zus is ouder dan ik')
+brother = Word('brother', 'broer', 'I play often with my brother', 'Ik speel vaak met mijn broer')
+grandfather = Word('grandfather', 'opa', 'Every sunday, I go to my grandfather', 'Elke zondag, ga ik naar mijn opa')
+grandmother = Word('grandmother', 'oma', 'My grandmother watches tv', 'Mijn oma kijkt tv')
+
+cat = Word('cat', 'kat', 'The cat is hungry', 'De kat heeft honger')
+dog = Word('dog', 'hond', 'The dog plays with a ball', 'De hond speelt met een bal')
+gorilla = Word('gorilla', 'gorilla', 'There is a gorilla in the woods', 'Er is een gorilla in het bos')
+mouse = Word('mouse', 'muis', 'The mouse eats some cheese', 'De muis eet wat kaas')
+fish = Word('fish', 'vis', 'There is a fish in the water', 'Er is een vis in het water')
+cow = Word('cow', 'koe', 'The cow does moooo', 'De koe doet moooo')
+elephant = Word('elephant', 'olifant', 'The elephant eats peanuts', 'De olifant eet pinda’s')
+
+transportation = Lesson('Transportation', [bike, car, train, plane, bus, subway])
+family = Lesson('Family', [mother, father, sister, brother, grandfather, grandmother])
+animals = Lesson('Animals', [cat, dog, gorilla, mouse, fish, cow, elephant])
+
+lessons = [transportation, family, animals]
+
+
+# pickle.dump({}, open('people_dict.txt', 'wb')) # run once to create a text file, then comment it out on any other run
 
 class DialogFlowSampleApplication(Base.AbstractApplication):
     def main(self):      
@@ -10,14 +135,14 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
         
         self.setDialogflowKey('test-agent-jpjots-d58ddb261ce0.json')
         self.setDialogflowAgent('test-agent-jpjots')
-        peopledict = pickle.load(open('peopledict.txt', 'rb'))
+        self.people_dict = pickle.load(open('people_dict.txt', 'rb'))
 
         self.speechLock = Semaphore(0)
         name = self.ask_question('Hello, what is your name?', 'answer_name')
 
-        if name not in people_dict.keys():
-            people_dict[name] = Person(name)
-        self.person = people_dict[name]
+        if name not in self.people_dict.keys():
+            self.people_dict[name] = Person(name)
+        self.person = self.people_dict[name]
         
         if self.person.firstlesson:
             self.sayAnimated2('Nice to meet you ' + self.person.name + '!')
@@ -34,7 +159,7 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
         Output: None
         '''
         self.sayAnimated(string)
-        self.speechLock.Acquire()
+        self.speechLock.acquire()
         
         
     def listen(self, context):
@@ -66,17 +191,19 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
         self.sayAnimated2(question)
         if switchlanguage:
             self.changelanguage()
+            print(self.language)
         answer = self.listen(context)
         if switchlanguage:
             self.changelanguage()
+            print(self.language)
         
         print(f'Question: {question}, Answer: {answer}')
         
-        if answer is not None:
+        if answer:
             return answer
         else:
             self.respond('no response')
-            self.ask_question(question, context)
+            self.ask_question(question, context, switchlanguage)
     
     
     def ask_to_repeat(self, word):
@@ -92,18 +219,20 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
         self.changelanguage()
 
         print(f'Word: {word}, Answer: {answer}')
-
+        self.counter += 1
+        print('counter', self.counter)
         if answer == word:
             self.respond('correct')
             if hasattr(self, 'lesson'):
                 self.lesson.correct_answer(word)
-        elif answer:
+        elif answer and self.counter < 3:
             self.respond('again')
             self.ask_to_repeat(word)
-        else:
+        elif answer:
+            self.sayAnimated2('Lets continue with the next word.')
+        elif self.counter < 3:
             self.respond('no response')
             self.ask_to_repeat(word)
-    
     
     def respond(self, type_):
         '''
@@ -130,8 +259,10 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
                     'Don\'t ignore me!', 
                     'Could you respond next time?', 
                     'It\'s not nice to ignore me!']}
-    
-        self.sayAnimated2(sample(options[type_], 1)[0])
+
+        response = random.sample(options[type_], 1)[0]
+        print(response)
+        self.sayAnimated2(response)
       
     
     def onRobotEvent(self, event):
@@ -155,7 +286,7 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
             self.answerLock.release()
 
 
-    def changeLanguage(self):
+    def changelanguage(self):
         '''
         Changes the language of the robot to dutch if it was in english and the other way around.
         Input: None
@@ -172,7 +303,6 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
             self.setLanguage('en-US')
             self.langLock.acquire()
             self.language = 'en-US'
-            self.sayAnimated2('Back to english')
 
             
     def firstlesson(self):
@@ -183,25 +313,43 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
         Output: None
         '''
         self.sayAnimated2('Today we will start learning the Dutch language together. I would like to greet you in Dutch.')
-        self.ask_to_repeat('goedenmiddag')
-
+        self.sayAnimated2('Try to repeat after me')
+        self.counter = 0
+        self.ask_to_repeat('goedemiddag')
+        self.sayAnimated2('This is it for our first intro lesson')
+        answer = self.ask_question('Do you want to start the next lesson', 'Yesno')
+        if answer == 'Yes':
+            self.startlesson()
+        else:
+            self.sayAnimated2('See you next time. I hope you enjoyed our time together')
         # something something
         
-        people_dict[self.person.name].firstlesson = False
+        self.people_dict[self.person.name].firstlesson = False
+        pickle.dump(self.people_dict, open('people_dict.txt', 'wb'))
     
     def recap_lesson(self, lesson):
+
         self.sayAnimated2('Let\'s start by going quickly through your previous lesson')
         
-        words = sample(lesson.learned_words, 3)
+        words = random.sample(lesson.learned_words, 3)
+        i = 0
         for word in words:
-            answer = self.ask_question(f'Do you remember the dutch translation for {word.word}?', word.translation, True)
+            options = [f'Can you tell me the dutch translation for {word.word}?',
+                       f'How do you say {word.word} in Dutch?',
+                       f'How about a word for {word.word} in Dutch?']
+
+            answer = self.ask_question(options[i], word.translation, True)
             if answer.lower() == word.translation.lower():
                 self.respond('correct')
+                #self.person.words_remembered.append(word)
             elif answer:
                 self.sayAnimated2(f'The correct translation of {word.word} is')
                 self.changelanguage()
                 self.sayAnimated2(word.translation)
                 self.changelanguage()
+
+            i -= -1
+        #print(self.person.words_remembered)
             
         
         
@@ -228,28 +376,39 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
             for lesson in self.person.lessons_to_learn:
                 if request.lower() == lesson.name.lower():
                     self.lesson = lesson
+                    self.sayAnimated2('Great, lets learn about' + lesson.name.lower())
 
             if self.lesson is None:   
-                self.lesson = sample(self.person.lessons_to_learn, 1)[0]
+                self.lesson = random.sample(self.person.lessons_to_learn, 1)[0]
 
             for i in range(len(self.lesson.words_to_learn)):
                 word = self.lesson.sample()
                 self.sayAnimated2(f'The next word means {word.word}')
+                self.counter = 0
                 self.ask_to_repeat(word.translation)
                 
-                if random.random() > .6:
+                if random.random() > .8:
                     self.sayAnimated2(f'Let\'s use {word.word} in a sentence')
                     self.sayAnimated2(word.sentence)
+                    self.sayAnimated2('Dont get scared, you dont need to repeat after me')
                     self.changelanguage()
-                    self.sayAniamted2(word.sentence_translation)
+                    self.sayAnimated2(word.sentence_translation)
                     self.changelanguage()
 
+            self.sayAnimated2('The lesson is done, your progress is saved')
             self.person.update()
+            self.people_dict[self.person.name] = self.person
+            pickle.dump(self.people_dict, open('people_dict.txt', 'wb'))
+            answer = self.ask_question('Do you want to start the next lesson', 'Yesno')
+            if answer == 'Yes':
+                self.startlesson()
+            else:
+                self.sayAnimated2('See you next time. I hope you enjoyed our time together')
         else:
             self.sayAnimated2('It looks like you\'ve already completed all the lessons!')            
 
             
 # Run the application
-#sample = DialogFlowSampleApplication()
-#sample.main()
-#sample.stop()
+sample = DialogFlowSampleApplication()
+sample.main()
+sample.stop()
